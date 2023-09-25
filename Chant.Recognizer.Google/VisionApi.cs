6f7d13470c48525f6b85eef929bae47c3b1b1c2d
xztaityozx <taityonohibi@gmail.com>
2023-09-25 21:05:44 +0900

    feat: OCRエンジンのインスタンスを都度生成できるように、ファクトリーのインターフェースを作った

--- Chant.Recognizer.Google/VisionApi.cs
+++ Chant.Recognizer.Google/VisionApi.cs
@@ -3,32 +3,64 @@ using Google.Cloud.Vision.V1;
 
 namespace Chant.Recognizer.Google;
 
+public class VisionApiFactory : IRecognizerFactory
+{
+    public IRecognizer Create()
+    {
+        return new VisionApi();
+    }
+}
+
 public class VisionApi : IRecognizer
 {
+    public int Rect { get; set; } = 100;
+
     /// <summary>
     /// Google Cloud Vision API を使って画像からテキストを抽出する
-    /// いまのところ横書きのみ…。
     /// </summary>
-    /// <param name="filePath"></param>
+    /// <param name="filePath">画像ファイルへのパス</param>
+    /// <param name="direction">文字の方向</param>
     /// <returns></returns>
-    public async Task<string> RecognizeAsync(string filePath)
+    public async Task<string> RecognizeAsync(string filePath, Direction direction)
     {
         var image = await Image.FromFileAsync(filePath);
         var client = await ImageAnnotatorClient.CreateAsync();
         var textAnnotation = await client.DetectDocumentTextAsync(image);
-        return string.Join(
-            "",
-            textAnnotation.Pages
-                .SelectMany(page => page.Blocks)
-                .SelectMany(block => block.Paragraphs)
-                .SelectMany(paragraph => paragraph.Words)
-                .SelectMany(word => word.Symbols)
-                // 100x100のグリッドに正規化して並び替える
-                // 普通にテキストとして取り出すとパラグラフがえらいことになりがちなので…
-                .OrderBy(symbol => symbol.BoundingBox.Vertices[0].Y / 100 * 100)
-                .ThenBy(symbol => symbol.BoundingBox.Vertices[0].X / 100 * 100)
-                .Select(a => a.Text)
-        );
+
+        // 並び替えないでそのまま取り出す。というのもRectを実行前に正確に計算するのが難しいっていうのがある
+        // Rectの大きさを実行前に調べておけばいいのだけど
+        // それってVisionAPIもやってるよねってなった(そしてVisionAPIのが高性能)
+        return textAnnotation.Text;
+
+        // LTのデモでやってたように100x100のグリッドが引かれた画像編集ソフトで書いた画像ならRectの効果は絶大
+        // VisionAPIが順番を間違えても、座標をもとに並び替えることで、必ず正しい順番になるから
+        //var words = textAnnotation.Pages
+        //    .SelectMany(page => page.Blocks)
+        //    .SelectMany(block => block.Paragraphs)
+        //    .SelectMany(paragraph => paragraph.Words)
+        //    .SelectMany(word => word.Symbols)
+        //    .Select(
+        //        symbol =>
+        //            new
+        //            {
+        //                symbol.Text,
+        //                X = symbol.BoundingBox.Vertices[0].X / Rect * Rect,
+        //                Y = symbol.BoundingBox.Vertices[0].Y / Rect * Rect
+        //            }
+        //    );
+
+        //// Rect * Rect のグリッドに正規化してからdirectionに従って並び替える
+        //// 普通にテキストとして取り出すと段落がガタガタになることがある
+        //return string.Join(
+        //    "",
+        //    direction == Direction.Vertical
+        //        ? words
+        //            .OrderByDescending(word => word.X) // 縦書きは右上から左下に並べることで再現できる
+        //            .ThenBy(word => word.Y)
+        //            .Select(word => word.Text)
+        //        // こちらは横書き
+        //        : words.OrderBy(word => word.Y).ThenBy(word => word.X).Select(word => word.Text)
+        //);
     }
 
     public string RecognizerName => "GoogleCloudVisionApi";
--- Chant.Recognizer.Shared/IRecognizer.cs
+++ Chant.Recognizer.Shared/IRecognizer.cs
@@ -1,7 +1,18 @@
 namespace Chant.Recognizer.Shared;
 
+public enum Direction
+{
+    Horizontal,
+    Vertical,
+}
+
 public interface IRecognizer
 {
-    public Task<string> RecognizeAsync(string filePath);
+    public Task<string> RecognizeAsync(string filePath, Direction direction);
     public string RecognizerName { get; }
+}
+
+public interface IRecognizerFactory
+{
+    public IRecognizer Create();
 }
